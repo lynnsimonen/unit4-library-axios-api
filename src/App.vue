a3ab15ac281a285208af2f796fb3d4ebf6fec4b8
lynn simonen <lsimonen@my.wctc.edu>
2023-04-26 10:59:39 -0500

    consume iTunes api and view on console and DOM

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,81 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div >
+    <h1>iTunes Media List</h1>
+    <div
+        v-for="item in info"
+        :key="item.artworkUrl100">
+      <p style="font-size: large; font-weight: bolder;">{{item.artistName}}:</p>
+      <div>{{ item.wrapperType }}</div>
+      <div>
+        <img src="{{ item.previewUrl }}" alt="album artwork" width="500" height="800">
+      </div>
+    </div>
+  </div>
+
+  <div class="container-fluid mt-3">
+    <div class="row row-cols-1 row-cols-sm-2 row-cols-lg-3 row-cols-xxl-4 g-3">
+      <div class="col" v-for="item in info" :key="item.artworkUrl100">
+        <div class="card h-100">
+          <div class="card-body">
+            <component :is="itemCardComponent(item)" :item="item"/>
+            <!--Decorator does NOT reference as "item.media"-->
+          </div>
+          <div class="card-footer d-flex justify-content-end">
+            <button v-if="item.isAvailable  && item.isAvailable() && item.checkOut"
+                    class="btn btn-outline-success" @click="item.checkOut()">Check Out</button>
+            <button v-else-if="item.checkIn" class="btn btn-outline-warning"
+                    @click="item.checkIn()">Check In</button>
+            <button class="btn btn-danger" @click="item.remove(item)">Delete</button>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import axios from "axios";
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+
+  },
+  data(){
+    return{
+      apiURL: 'https://itunes.apple.com/search?term=mash',
+      info: null,
+      loading: true,
+      errored: false,
+      searchTerm: 'U2',
+      axiosParams: {
+        limit: 36
+        ,
+      }
+    }
+  },
+  methods: {
+    itemCardComponent(item){
+      //return item.constructor.name + 'Card'; //will fail with webpack
+      return item.constructor.type + 'Details';
+      //Decorator does NOT reference item.media.constructor.type
+    }
+  },
+  created(){
+    console.log('Library Item Card is: ' + Object +  typeof Object)
+  },
+  mounted() {
+    axios
+        .get(this.apiURL,{params: this.axiosParams})
+        .then(response => {
+          this.info = response.data.results
+          console.log(this.info)
+        })
+        .catch(error => {
+          console.log(error)
+          this.errored = true
+        })
+        .finally(() => this.loading = false)
   }
 }
 </script>
---
+++ src/models/LibraryItem.js
@@ -0,0 +1,26 @@
+export default function LibraryItem(media) {
+
+    const STATUSES =
+        {CHECKED_IN: 'in', CHECKED_OUT: 'out', ON_HOLD: 'hold'}
+    //let in2 = 'in';
+
+    //add library item functionality to the book or movie (media)media
+
+    // set the default status
+    media.status = STATUSES.CHECKED_IN;
+
+    // methods
+    media.checkIn = function () {
+        this.status = STATUSES.CHECKED_IN;
+    }
+
+    media.checkOut = function () {
+        this.status = STATUSES.CHECKED_OUT;
+    }
+
+    media.isAvailable = function () {
+        return this.status === STATUSES.CHECKED_IN;
+    }
+
+    return media;
+}
